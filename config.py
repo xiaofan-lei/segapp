956dcf202758e2065c1b43d87529e082bb27f87b
xiaofan-lei <90057424+xiaofan-lei@users.noreply.github.com>
2022-05-11 10:24:10 +0200

    Update config.py
    remove ids

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-TENANT_ID = 'b7f12377-8b58-428a-84d4-37099d4cabbc'
-APP_ID = '328cb245-19c2-4d90-bcf4-8970f7900f00'
-SVR_PW = 'V3q7Q~HYHXZ7qshA90FIlRrX~wpKlTqXkLGfR'
-SUBSCRIPTION_ID = '403c34e4-adde-4596-85b1-272a798d7ef2'
-RESOURCE_GROUP = 'Openclassrooms'
-WS_NAME = 'docs-ws'
+TENANT_ID = 'tenant_id'
+APP_ID = 'app_id'
+SVR_PW = 'password'
+SUBSCRIPTION_ID = 'sub_id'
+RESOURCE_GROUP = 'groupname'
+WS_NAME = 'wsname'
 
 #get images form file dataset
-DATASET_URL='https://docsws7974068583.blob.core.windows.net/azureml-blobstore-2ee489bf-29a7-40e9-a468-4950801c3cc6/'
-IMG_URL = DATASET_URL + 'UI/04-15-2022_073440_UTC/citydata/leftImg8bit/test/bonn/'
-TARGET_PATH = 'UI/04-15-2022_073440_UTC/citydata/gtFine/test'
+DATASET_URL='https://xxx/'
+IMG_URL = DATASET_URL + 'UI/xxxx/cityscapes/leftImg8bit/test/bonn/'
+TARGET_PATH = 'UI/xxxx/cityscapes/gtFine/test'
 IMG_LIST = ['bonn_000000_000019_leftImg8bit.png',
            'bonn_000001_000019_leftImg8bit.png',
            'bonn_000002_000019_leftImg8bit.png',
